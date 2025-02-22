fa04b4aeb689b4546eb03eb446d130afb31aeac4
Tomiamsy <tomiams.gaming@gmail.com>
2025-02-22 18:52:52 +0100

    Ball experiments

---
+++ My project/Assets/Ballcontroller.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpForce = 10.0f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+}
